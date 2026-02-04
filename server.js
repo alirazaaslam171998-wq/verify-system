c7ee180e93c55da9ad7f92fac47d924774d688fd
Ali Raza <alirazaaslam171998@gmail.com>
2026-02-04 14:10:58 +0000

    Initial commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const session = require("express-session");
+const svgCaptcha = require("svg-captcha");
+const path = require("path");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "training-secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(express.static("public"));
+app.use("/docs", express.static("docs"));
+
+app.get("/api/captcha", (req, res) => {
+  const captcha = svgCaptcha.create({
+    size: 6,
+    noise: 3,
+    background: "#f2f2f2",
+  });
+
+  req.session.captcha = captcha.text;
+  res.type("svg");
+  res.send(captcha.data);
+});
+
+app.post("/api/verify", (req, res) => {
+  const { captcha, docId } = req.body;
+
+  if (!captcha || captcha !== req.session.captcha) {
+    return res.json({ success: false });
+  }
+
+  res.json({
+    success: true,
+    documentUrl: `/docs/sample.pdf`,
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
