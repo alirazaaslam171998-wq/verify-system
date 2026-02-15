ff71359a3f7635afe3383f9ee26b94428281cf74
alirazaaslam171998-wq <alirazaaslam171998@gmail.com>
2026-02-15 15:41:16 +0000

    Update script_new.js
    NEW QRR

--- public/script_new.js
+++ public/script_new.js
@@ -10,7 +10,8 @@ async function searchDoc() {
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
       captcha: captcha,
-      docId: "TRAINING-ID"
+      docId: "newcert"
+
     })
   });
 
