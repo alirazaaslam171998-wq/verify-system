2db26654f7647730660beb324d4952cf0e188bf8
alirazaaslam171998-wq <alirazaaslam171998@gmail.com>
2026-04-10 10:20:54 +0100

    Death certificate added

--- server.js
+++ server.js
@@ -49,6 +49,9 @@ app.post("/api/verify", (req, res) => {
   } else if (docId === "newcert") {
     // New certificate
     documentUrl = "/docs/marriage_certificate_es78524612.pdf";
+    } else if (docId === "deathcert") {
+  documentUrl = "/docs/death_certificate_es17154778.pdf";
+}
   } else {
     return res.json({ success: false });
   }
